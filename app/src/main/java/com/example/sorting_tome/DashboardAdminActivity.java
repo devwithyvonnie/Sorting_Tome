13c72e0e653685bc7840ab37b0be9a5aa00d8f0c
AAGross <102828654+AAGross@users.noreply.github.com>
2022-12-16 03:43:17 -0500

    Show Book Categories

--- app/src/main/java/com/example/sorting_tome/DashboardAdminActivity.java
+++ app/src/main/java/com/example/sorting_tome/DashboardAdminActivity.java
@@ -1,20 +1,35 @@
 package com.example.sorting_tome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 
 import com.example.sorting_tome.databinding.ActivityDashboardAdminBinding;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DashboardAdminActivity extends AppCompatActivity {
 
     private ActivityDashboardAdminBinding binding;
 
     private FirebaseAuth firebaseAuth;
+
+    //arrayList to store category
+    private ArrayList<ModelCategory> categoryArrayList;
+    //adapter
+    private AdapterCategory adapterCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,31 @@ public class DashboardAdminActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+        loadCategories();
+
+        //edit text change listen, search
+        binding.searchEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //called as and when user types each letter
+                try {
+                    adapterCategory.getFilter().filter(s);
+                }
+                catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         binding.loginBtn.setOnClickListener(new View.OnClickListener(){
        @Override
@@ -32,8 +72,6 @@ public class DashboardAdminActivity extends AppCompatActivity {
            firebaseAuth.signOut();
            checkUser();
        }
-
-
     });
 
         binding.addCategoryBtn.setOnClickListener(new View.OnClickListener()
@@ -43,6 +81,38 @@ public class DashboardAdminActivity extends AppCompatActivity {
             {
                 startActivity(new Intent(DashboardAdminActivity.this,
                         CategoryAddActivity.class));
+            }
+        });
+    }
+
+    private void loadCategories() {
+        //init arrayList
+        categoryArrayList = new ArrayList<>();
+
+        //get all categories from firebase > Categories
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //clear arrayList before adding data into it
+                categoryArrayList.clear();
+                for (DataSnapshot ds: snapshot.getChildren()){
+                    //get data
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+
+                    //add to arrayList
+                    categoryArrayList.add(model);
+                }
+                //setup adapter
+                adapterCategory = new AdapterCategory(DashboardAdminActivity.this, categoryArrayList);
+                //set adapter to recyclerview
+                binding.categoriesRv.setAdapter(adapterCategory);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
@@ -59,7 +129,5 @@ public class DashboardAdminActivity extends AppCompatActivity {
             binding.subTitleTv.setText(email);
 
         }
-
     }
-
 }
