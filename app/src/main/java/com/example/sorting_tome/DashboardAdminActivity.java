5ff9b4d5cf2fd96ab484250f4990d67c4c5e4041
Dperez007 <danielperez078@gmail.com>
2023-01-10 22:58:52 -0500

    upload pdf

--- app/src/main/java/com/example/sorting_tome/DashboardAdminActivity.java
+++ app/src/main/java/com/example/sorting_tome/DashboardAdminActivity.java
@@ -83,7 +83,21 @@ public class DashboardAdminActivity extends AppCompatActivity {
                         CategoryAddActivity.class));
             }
         });
+
+        binding.addPdfFab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent (DashboardAdminActivity.this, PdfAddActivity.class));
+            }
+
+        });
+
+
     }
+
+
+
 
     private void loadCategories() {
         //init arrayList
---
+++ app/src/main/java/com/example/sorting_tome/PdfListAdminActivity.java
@@ -0,0 +1,14 @@
+package com.example.sorting_tome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PdfListAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_list_admin);
+    }
+}
