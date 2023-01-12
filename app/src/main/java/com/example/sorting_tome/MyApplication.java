f3050a5fb55e8b6df9e9adcdbf3260468f324577
devwithyvonnie@gmail.com <devwithyvonnie@gmail.com>
2023-01-11 19:25:44 -0700

    Continued with ModelPdf, Added AdapterPdfAdmin, implemented PDF Viewer.

--- app/src/main/java/com/example/sorting_tome/MyApplication.java
+++ app/src/main/java/com/example/sorting_tome/MyApplication.java
@@ -6,10 +6,8 @@ public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
-         super.onCreate();
+        super.onCreate();
     }
-
-    public static final
 }
 
 
