52be5852d0660aac902a8a2b639ab7d3c7b831c2
devwithyvonnie@gmail.com <devwithyvonnie@gmail.com>
2023-01-14 15:32:25 -0700

    Fixed Majority Issue - Will come back and fix next week

--- app/src/main/java/com/example/sorting_tome/PdfAddActivity.java
+++ app/src/main/java/com/example/sorting_tome/PdfAddActivity.java
@@ -1,14 +1,317 @@
 package com.example.sorting_tome;
 
+import static android.content.ContentValues.TAG;
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import com.example.sorting_tome.models.ModelCategory;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.example.sorting_tome.databinding.ActivityPdfAddBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PdfAddActivity extends AppCompatActivity {
+
+    private ActivityPdfAddBinding binding;
+
+
+
+    //FirebaseAuth
+    private FirebaseAuth firebaseAuth;
+
+    private ProgressDialog progressDialog;
+
+    private ArrayList<ModelCategory> categoryArrayList;
+
+  private Uri pdfUri = null;
+
+  private static final int PDF_PICK_CODE = 1000;
+
+    //Tag for bug
+    private static final String Tag = "ADD_PDF_Tag";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pdf_add);
+        binding = ActivityPdfAddBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        loadPdfCategories();
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("PLease wait");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        binding.attachBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+                public void onClick(View v){
+              pdfPickIntent();
+            }
+        });
+
+        binding.categoryTv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v ){
+            categoryPickDialog();
+
+        }
+        });
+
+        binding.submitBtn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v)
+            {
+                validateData();
+            }
+
+        });
+
     }
-}
+private String title = "",description = "", category = "";
+    private void validateData(){
+        Log.d(TAG, "validateData: validating data...");
+
+//get data
+        title = binding.titleEt.getText().toString().trim();
+        description = binding.descriptionTilEt.getText().toString().trim();
+        category = binding.categoryTv.getText().toString().trim();
+//validate data
+
+        if(TextUtils.isEmpty(title)){
+            Toast.makeText(this,"Enter Title...", Toast.LENGTH_SHORT).show();
+
+
+        }
+        else if(TextUtils.isEmpty(description)){
+            Toast.makeText(this,"Enter Description...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(category)){
+            Toast.makeText(this,"Pick Category...", Toast.LENGTH_SHORT).show();
+        }
+        else if(pdfUri == null){
+            Toast.makeText(this,"Pick Pdf...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            uploadPdfToStorage();
+
+        }
+    }
+
+    private void uploadPdfToStorage(){
+        Log.d(TAG, "uploadPdfToStorage: upload to storage...");
+//show progress
+        progressDialog.setMessage("Upload Pdf...");
+        progressDialog.show();
+
+        long timestamp = System.currentTimeMillis();
+        String filePathAndName = "Books/" +timestamp;
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference(filePathAndName);
+        storageReference.putFile(pdfUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.d(TAG, "onSuccess: PDF uploaded to storage...");
+                        Log.d(TAG, "onSuccess: getting pdf url...");
+
+
+                        //get pdf url
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uriTask.isSuccessful());
+                        String uploadPdfUrl = ""+ uriTask.getResult();
+                        //upload to firebase db
+                        uploadPdfInfoToDB(uploadPdfUrl, timestamp);
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Log.d(TAG, "onFailure: PDF upload failed due" + e.getMessage());
+                        Toast.makeText(PdfAddActivity.this,  "PDF upload failed due to"+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void uploadPdfInfoToDB(String uploadPdfUrl, long timestamp) {
+        // upload Pdf info to firebase db
+        Log.d(TAG, "uploadPdfToStorage: uploading Pdf to firebase db...");
+
+        progressDialog.setMessage("Uploading pdf info");
+        String uid = firebaseAuth.getUid();
+        // setup Data
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("uid", ""+uid);
+        hashMap.put("id", ""+timestamp);
+        hashMap.put("description", ""+description);
+        hashMap.put("url", ""+uploadPdfUrl);
+        hashMap.put("category", ""+category);
+        hashMap.put("timestamp", ""+timestamp);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Books");
+        ref.child(""+timestamp)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        progressDialog.dismiss();
+                        Log.d(TAG, "OnSuccess: Successfully uploaded...");
+                        Toast.makeText(PdfAddActivity.this, "Succssfuly uploaded...", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Log.d(TAG, "onFailure: Failed to upload to db due to "+e.getMessage());
+                        Toast.makeText(PdfAddActivity.this, "Failed to upload to db due to"+e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+
+
+
+
+
+
+
+
+
+    }
+
+    private void pdfPickIntent(){
+        Log.d(Tag, "pdfPickIntent: starting pdf pick intent");
+
+       Intent intent = new Intent();
+       intent.setType("application/pdf");
+       intent.setAction(Intent.ACTION_GET_CONTENT);
+
+       startActivityForResult(Intent.createChooser(intent, "Select PDF"), PDF_PICK_CODE);
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if(requestCode == PDF_PICK_CODE){
+            Log.d(TAG, "onActivityResult: PDF Picked");
+
+            pdfUri = data.getData();
+            Log.d(TAG, "onActivityResult: URI;" + pdfUri);
+
+        }
+    }
+
+    else
+        {
+            Log.d(TAG, "onActivityResult: Cancelled picking pdf");
+            Toast.makeText(this, "cancelled picking pdf",Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+
+
+
+    private void loadPdfCategories() {
+        Log.d(TAG, "loadPdfCategories: Loading pdf categories...");
+        categoryArrayList = new ArrayList<>();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                categoryArrayList.clear();//clear before adding data
+                for(DataSnapshot ds: snapshot.getChildren()){
+
+                    ModelCategory model = ds.getValue(ModelCategory.class);
+                    categoryArrayList.add(model);
+                    Log.d(TAG, "onDataChange: "+model.getCategory());
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
+    private void categoryPickDialog() {
+        Log.d(TAG, "categoryPickDialog: showing category pick dialog");
+        String[] categoriesArray = new String[  categoryArrayList.size()];
+        for(int i =0; i<categoryArrayList.size();i++) {
+            categoriesArray[i] = categoryArrayList.get(i).getCategory();
+
+
+        }
+
+        //alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick Category")
+                .setItems(categoriesArray, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //handle item click
+                        //get clicked item from list
+                        String category = categoriesArray[which];
+                        binding.categoryTv.setText(category);
+
+                        Log.d(TAG, "onClick: Selected Category"+ category);
+                    }
+                })
+                .show();
+
+    }
+
+    }
--- app/src/main/java/com/example/sorting_tome/adapters/AdapterPdfAdmin.java
+++ app/src/main/java/com/example/sorting_tome/adapters/AdapterPdfAdmin.java
@@ -1,39 +1,65 @@
 package com.example.sorting_tome.adapters;
 
+import static com.example.sorting_tome.Constants.MAX_BYTES_PDF;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.sorting_tome.MyApplication;
 import com.example.sorting_tome.databinding.RowPdfAdminBinding;
+import com.example.sorting_tome.filters.FilterPdfAdmin;
 import com.example.sorting_tome.models.ModelPdf;
 import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.listener.OnErrorListener;
+import com.github.barteksc.pdfviewer.listener.OnPageErrorListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageMetadata;
+import com.google.firebase.storage.StorageReference;
 
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 
-
-public class AdapterPdfAdmin extends RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin> {
+public class AdapterPdfAdmin extends RecyclerView.Adapter<AdapterPdfAdmin.HolderPdfAdmin> implements Filterable {
     //Context
     private Context context;
 
     //arrayList to hold list of data of type ModelPdf
-    private ArrayList<ModelPdf> pdfArrayList;
+    public ArrayList<ModelPdf> pdfArrayList;
+    private ArrayList<ModelPdf> filterList;
 
     //view binding row_pdf_admin.xml
     private RowPdfAdminBinding binding;
+
+    private static final String TAG = "PDF_ADAPTER_TAG";
+
+    private FilterPdfAdmin filter;
 
     //constructor
     public AdapterPdfAdmin(Context context, ArrayList<ModelPdf> pdfArray) {
         this.context = context;
         this.pdfArrayList = pdfArrayList;
+        this.filterList = pdfArrayList;
     }
 
     @NonNull
@@ -54,11 +80,133 @@ public class AdapterPdfAdmin extends RecyclerView.Adapter<AdapterPdfAdmin.Holder
         String title = model.getTitle();
         String description = model.getDescription();
         long timestamp = model.getTimestamp();
+
+        //converting timestamp to MM/dd/yyyy
+        String formattedDate = MyApplication.formatTimestamp(timestamp);
+
+        //set data
+        holder.titleTv.setText(title);
+        holder.descriptionTv.setText(description);
+        holder.dateTv.setText(formattedDate);
+
+        //load further details like category, pdf from url, pdf size in separate function
+        loadCategory(model, holder);
+        loadPdfFromUrl(model, holder);
+        loadPdfSize(model, holder); 
+
+
+    }
+
+    private void loadPdfSize(ModelPdf model, HolderPdfAdmin holder) {
+        //using url we can get file and its metadata from firebase storage
+        String pdfUrl = model.getUrl();
+
+        StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl);
+        ref.getMetadata()
+                .addOnSuccessListener(new OnSuccessListener<StorageMetadata>() {
+                    @Override
+                    public void onSuccess(StorageMetadata storageMetadata) {
+                        //get size in bytes
+                        double bytes = storageMetadata.getSizeBytes();
+                        Log.d(TAG, "onSuccess: "+model.getTitle() + " "+bytes);
+
+                        //convert bytes to KB / MB
+                        double kb = bytes/1024;
+                        double mb = kb/1024;
+
+                        if (mb >= 1) {
+                            holder.sizeTv.setText(String.format("%.2f", mb)+" MB");
+                        } else if (kb >= 1) {
+                            holder.sizeTv.setText(String.format("%.2f", kb)+" KB");
+                        } else {
+                            holder.sizeTv.setText(String.format("%.2f", bytes)+" bytes");
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed getting metadata
+                        Log.d(TAG, "onFailure: "+e.getMessage());
+                    }
+                });
+    }
+
+    private void loadPdfFromUrl(ModelPdf model, HolderPdfAdmin holder) {
+        //using url we can get file and its metadata from firebase storage
+
+        String pdfUrl = model.getUrl();
+        StorageReference ref = FirebaseStorage.getInstance().getReferenceFromUrl(pdfUrl);
+        ref.getBytes(MAX_BYTES_PDF)
+                .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                    @Override
+                    public void onSuccess(byte[] bytes) {
+                        Log.d(TAG, "onSuccess: "+model.getTitle()+" successfully for the file");
+
+                        //set to pdfview
+                        holder.pdfView.fromBytes(bytes)
+                                .pages(0) //shows only first page
+                                .spacing(0)
+                                .swipeHorizontal(false)
+                                .enableSwipe(false)
+                                .onError(new OnErrorListener() {
+                                    @Override
+                                    public void onError(Throwable t) {
+                                        Log.d(TAG, "onError: "+t.getMessage());
+                                    }
+                                })
+                                .onPageError(new OnPageErrorListener() {
+                                    @Override
+                                    public void onPageError(int page, Throwable t) {
+                                        Log.d(TAG, "onPageError: "+t.getMessage());
+                                    }
+                                })
+                                .load();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: Failed to get file from url due to "+e.getMessage());
+                    }
+                });
+    }
+
+    private void loadCategory(ModelPdf model, HolderPdfAdmin holder) {
+        //get category using categoryId
+
+        String categoryId = model.getCategoryId();
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Categories");
+        ref.child(categoryId)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        //get category
+                        String category = ""+snapshot.child("category").getValue();
+
+                        //set to category text view
+                        holder.categoryTv.setText(category);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
     }
 
     @Override
     public int getItemCount() {
         return pdfArrayList.size(); // return number of records / list size
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (filter == null) {
+            filter = new FilterPdfAdmin(filterList, this);
+        }
+        return filter;
     }
 
     /*---View Holder Class for row_pdf_admin.xml---*/
